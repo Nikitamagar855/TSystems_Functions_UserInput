1a46c4b34359d52fb56e32764541f6a96e656cb7
Nikitamagar855 <89832430+Nikitamagar855@users.noreply.github.com>
2022-03-24 11:31:32 +0530

    Add project files.

---
+++ TSystems_Functions_UserInput/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TSystems_Functions_UserInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter First Number");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please Enter Second Number");
+            int b = Convert.ToInt32(Console.ReadLine());
+            ArithmaticOp objArithmaticOp = new ArithmaticOp();
+            objArithmaticOp.add(a,b);
+            objArithmaticOp.sub(a, b);
+            objArithmaticOp.mul(a, b);
+            objArithmaticOp.div(a, b);
+            Console.ReadLine();
+
+
+
+        }
+    }
+
+
+    public class ArithmaticOp
+    {
+        public void  add(int a,int b)
+        {
+            Console.WriteLine($"Addition is { a + b}" );
+            
+        }
+        public void sub(int a, int b)
+        {
+            Console.WriteLine($"Substraction is { a - b}" );
+
+        }
+        public void mul(int a, int b)
+        {
+            Console.WriteLine($"multiplication  is { a * b}");
+
+        }
+        public void div(int a, int b)
+        {
+            Console.WriteLine($"Division  is { a/ b}");
+
+        }
+    }
+}
